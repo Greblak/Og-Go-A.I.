234bfdcefef2a063c6365ff79b7458207560d496
Greblak <greblak@gmail.com>
2013-03-06 23:43:38 +0100

    Indentation standardisation

--- src/SGFEngine.h
+++ src/SGFEngine.h
@@ -10,7 +10,7 @@
 
 class SGFEngine
 {
- public:
+public:
   static inline void generateFile(const GoGame* game)
   {
     std::ofstream file;
@@ -23,27 +23,25 @@ class SGFEngine
     file << "RU[Japanese]" << std::endl;
     file << "KM[0.5]" << std::endl;
     file << "SZ["<<game->Board->Size()<<"]" << std::endl;
+    //End of premable. LET'S PLAY!
 
     for (GoMoveIterator it = game->moves.begin(); it != game->moves.end(); ++it)
       {
-	char x = GTPEngine::ColumnIntToString((*it)->Point.x);
-	x = tolower(x);
-	int i_y = game->Board->Size() - ((*it)->Point.y) - 1; //Reverses due to odd notarion in SGF
-	char y = GTPEngine::ColumnIntToString(i_y);
-	y = tolower(y);
-	y = y > 'i' ? y-1 : y;
-	x = x > 'i' ? x-1 : x;
-	LOG_DEBUG << "X: "<<(*it)->Point.x<<" " << x;
-	LOG_DEBUG << "Y: "<<(*it)->Point.y<<" " << y;
-	LOG_DEBUG << "Color: "<<(*it)->Color;
+        char x = GTPEngine::ColumnIntToString((*it)->Point.x);
+        x = tolower(x);
+        int i_y = game->Board->Size() - ((*it)->Point.y) - 1; //Reverses due to odd notation in SGF
+        char y = GTPEngine::ColumnIntToString(i_y);
+        y = tolower(y);
+        y = y > 'i' ? y-1 : y;
+        x = x > 'i' ? x-1 : x;
 
-	char col = (*it)->Color == S_BLACK ? 'B' : 'W';
-    file << ";"<<col<<"["<<x<<y<<"]" <<std::endl;
-    char separator = it == game->moves.begin() ? ' ' : ';';
-LOG_DEBUG <<"Output to SGF:" << separator <<col<<"["<<x<<y<<"]" <<std::endl;
+        char col = (*it)->Color == S_BLACK ? 'B' : 'W';
+        file << ";"<<col<<"["<<x<<y<<"]" <<std::endl;
+        char separator = it == game->moves.begin() ? ' ' : ';';
+        LOG_DEBUG <<"Output to SGF:" << separator <<col<<"["<<x<<y<<"]" <<std::endl;
       }
 
-					      //End file
+    //End file
     file << ")" <<std::endl;
   }
 };

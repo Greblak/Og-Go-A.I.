4439c9b03d57a885ebbf5e226d783a876419b392
Greblak <greblak@gmail.com>
2013-03-10 13:50:53 +0100

    Added simple eye checker to prevent playing in your own eyes. Does not differentiate between real and false eyes yet

--- src/GoGame.cpp
+++ src/GoGame.cpp
@@ -37,11 +37,21 @@ GoPoint GoGame::GenerateMove(int color)
       ++counter;
       if(Board->IsLegal(point, point.color)) //Legal move. Do more checks
         {
+          if(
+              (Board->North(point) == -1 || Board->State.stones[Board->North(point)] == point.color+1)
+              && (Board->South(point) == -1 || Board->State.stones[Board->South(point)] == point.color+1)
+              && (Board->West(point) == -1 || Board->State.stones[Board->West(point)] == point.color+1)
+              && (Board->East(point) == -1 || Board->State.stones[Board->East(point)] == point.color+1)
+          )//Placing in own eye. Dumb move.
+            {
+              LOG_DEBUG << "DONT WANNA PLAY EYE AT "<<point.x<<","<<point.y;
+              continue;
+            }
           break;
         }
     }
-  while(counter < 361);
-  if(counter == 361)
+  while(counter < 1000);
+  if(counter == 1000)
     {
       LOG_DEBUG << "No valid moves left";
       return GoPoint(-1,-1,NONE);

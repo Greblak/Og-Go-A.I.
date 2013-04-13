783fd14072bae4bf9fd25ea9cde2c9aa7c6aed04
Greblak <greblak@gmail.com>
2013-04-13 13:16:26 +0200

    Fixed indentation

--- src/MasterServer.cpp
+++ src/MasterServer.cpp
@@ -22,202 +22,206 @@
 MasterServer::MasterServer(int port):tcp_server(io_service,port,boost::bind(&MasterServer::newConnection,this, _1)),writingToUcbTable(false)
 {
 
-	try
-	{
-		boost::thread workerThread(boost::bind(&boost::asio::io_service::run, &io_service));
-	}
-	catch (std::exception& e)
-	{
-		LOG_ERROR << e.what() << std::endl;
-	}
+  try
+    {
+      boost::thread workerThread(boost::bind(&boost::asio::io_service::run, &io_service));
+    }
+  catch (std::exception& e)
+    {
+      LOG_ERROR << e.what() << std::endl;
+    }
 }
 
 MasterServer::~MasterServer() {
-	io_service.stop();
+  io_service.stop();
 }
 
 void MasterServer::run()
 {
-	while(true)
+  while(true)
+    {
+      try
 	{
-		try
-		{
-			std::string userInput;
-			std::getline(std::cin, userInput);
-			std::vector<std::string> args = gtp.parse(userInput);
-			if(args[0] == "genmove")
-			{
-				GoPoint p = generateMove(gtp.ColorFromString(args[1]));
-				LOG_OUT<< "= "<<gtp.game->Board->ReadablePosition(gtp.game->Board->Pos(p));
-			}
-		}
-		catch( const char * str )
-		{
-			LOG_ERROR <<"? Fatal exception raised: "<<str<<"\n\n"<<"Exiting...";
-			exit(EXIT_FAILURE);
-		}
-		catch (Exception& e)
-		{
-			LOG_ERROR <<"? Fatal exception raised: "<<e.getMessage()<<"\n\n"<<"Exiting...";
-			exit(EXIT_FAILURE);
-		}
+	  std::string userInput;
+	  std::getline(std::cin, userInput);
+	  std::vector<std::string> args = gtp.parse(userInput);
+	  if(args[0] == "genmove")
+	    {
+	      GoPoint p = generateMove(gtp.ColorFromString(args[1]));
+	      LOG_OUT<< "= "<<gtp.game->Board->ReadablePosition(gtp.game->Board->Pos(p));
+	    }
 	}
+      catch( const char * str )
+	{
+	  LOG_ERROR <<"? Fatal exception raised: "<<str<<"\n\n"<<"Exiting...";
+	  exit(EXIT_FAILURE);
+	}
+      catch (Exception& e)
+	{
+	  LOG_ERROR <<"? Fatal exception raised: "<<e.getMessage()<<"\n\n"<<"Exiting...";
+	  exit(EXIT_FAILURE);
+	}
+    }
 }
 
 void MasterServer::newConnection(boost::shared_ptr<TCPConnection> conn)
 {
-	LOG_VERBOSE<<"New connection from "<<conn->socket().remote_endpoint().address().to_string()<<std::endl;
-	doHandshake(conn);
-	if(conn->socket().is_open())
-	{
-		sockets.push_back(conn);
-	}
+  LOG_VERBOSE<<"New connection from "<<conn->socket().remote_endpoint().address().to_string()<<std::endl;
+  doHandshake(conn);
+  if(conn->socket().is_open())
+    {
+      sockets.push_back(conn);
+    }
 
 }
 
 void MasterServer::doHandshake(boost::shared_ptr<TCPConnection> conn)
 {
-	conn->socket().write_some(boost::asio::buffer(EGTP_HANDSHAKE));
-	//	socket_.write_some(boost::asio::buffer(EGTP_HANDSHAKE));
-	boost::array<char, MASTER_BUFFER_MAX_SIZE> buf;
-	boost::system::error_code ec;
-	conn->socket().read_some(boost::asio::buffer(buf),ec);
-	if(std::string(buf.data(),strlen(buf.data())) != GTP_ACK_RESPONSE)
-	{
-		LOG_DEBUG<<"Invalid EGTP version or malformed response. Closing connection"<<std::endl;
-		conn->socket().close();
-	}
+  conn->socket().write_some(boost::asio::buffer(EGTP_HANDSHAKE));
+  //	socket_.write_some(boost::asio::buffer(EGTP_HANDSHAKE));
+  boost::array<char, MASTER_BUFFER_MAX_SIZE> buf;
+  boost::system::error_code ec;
+  conn->socket().read_some(boost::asio::buffer(buf),ec);
+  if(std::string(buf.data(),strlen(buf.data())) != GTP_ACK_RESPONSE)
+    {
+      LOG_DEBUG<<"Invalid EGTP version or malformed response. Closing connection"<<std::endl;
+      conn->socket().close();
+    }
 }
 
 void MasterServer::writeAll(const std::string str)
 {
-	for(SocketVector::iterator it = sockets.begin(); it != sockets.end(); ++it)
+  for(SocketVector::iterator it = sockets.begin(); it != sockets.end(); ++it)
+    {
+      if((**it).socket().is_open())
 	{
-		if((**it).socket().is_open())
-		{
-			//			std::cout<<"Trying to write to "<<&(**it);
-			try
-			{
-				write((*it),str);
-			}
-			catch(char* ex)
-			{
-				LOG_ERROR<<ex<<std::endl;
-			}
-		}
-		else
-		{
-			it = sockets.erase(it); //Remove dead socket
-		}
+	  //			std::cout<<"Trying to write to "<<&(**it);
+	  try
+	    {
+	      write((*it),str);
+	    }
+	  catch(char* ex)
+	    {
+	      LOG_ERROR<<ex<<std::endl;
+	    }
 	}
+      else
+	{
+	  it = sockets.erase(it); //Remove dead socket
+	}
+    }
 }
 
 void MasterServer::write(boost::shared_ptr<TCPConnection> conn, const std::string str)
 {
-	//	std::cout<<"Trying to write "<<str<<std::endl;
-	conn->socket().async_write_some(boost::asio::buffer(str),boost::bind(&MasterServer::writeHandler,this));
-	//	std::cout<<"Successfully wrote "<<str<<std::endl;
+  //	std::cout<<"Trying to write "<<str<<std::endl;
+  conn->socket().async_write_some(boost::asio::buffer(str),boost::bind(&MasterServer::writeHandler,this));
+  //	std::cout<<"Successfully wrote "<<str<<std::endl;
 }
 
 const GoPoint MasterServer::generateMove(int color)
 {
-	UpperConfidence ucb;
-	std::vector<int> randMoves = UpperConfidence().getPossibleMoves(color,gtp.game);
-	ucbTable.clear();
-	for(int i = 0; i<randMoves.size(); ++i)
+  UpperConfidence ucb;
+  std::vector<int> randMoves = UpperConfidence().getPossibleMoves(color,gtp.game);
+  ucbTable.clear();
+  for(int i = 0; i<randMoves.size(); ++i)
+    {
+      UCBrow r;
+      r.pos = randMoves[i];
+      r.expected = 0;
+      r.timesPlayed = 0;
+      ucbTable.push_back(r);
+    }
+
+  //Replay current state to all slaves
+  writeAll(GTP_CMD_CLEAR_BOARD);
+  for(int j = 0; j<gtp.game->Board->movePointer; ++j)
+    {
+      char col = gtp.game->Board->moves[j]->Color == S_BLACK ? 'b' : 'w';
+      std::stringstream ss;
+      ss<<"play "<<col<<" "<<gtp.game->Board->ReadablePosition(gtp.game->Board->moves[j]->Point)<<"\n";
+
+      writeAll(ss.str());
+    }
+
+  std::string wbuf = GTPEngine::generateGTPString(gtp.game->Board);
+  writeAll(wbuf);
+  std::stringstream ss;
+  ss<<"e_randmoves";
+  for(int j = 0;j<randMoves.size();++j)
+    ss<<" "<<randMoves[j];
+  ss<<"\n";
+  writeAll(ss.str());
+  wbuf = "e_useai ucb 0 s 200\ngenmove b";
+  writeAll(wbuf);
+
+  int moveNumber = gtp.game->Board->movePointer; //Implement to prevent race conditions
+
+  for(SocketVector::iterator it = sockets.begin(); it!= sockets.end(); ++it)
+    {
+      //Build errors
+      boost::array<char, MASTER_BUFFER_MAX_SIZE>* buf = new boost::array<char, MASTER_BUFFER_MAX_SIZE>();
+      buf->fill('\0');
+      //		socketreads.push_back(buf);
+      (*it)->socket().async_read_some(boost::asio::buffer(*buf),boost::bind(&MasterServer::genmoveReadCallback,this,(*it),buf));
+    }
+
+  sleep(10);
+
+  int bestPos = -1;
+  float bestExpected = 0;
+  int totalSims = 0;
+  for(int i = 0; i<ucbTable.size(); ++i)
+    {
+      if(ucbTable[i].expected > bestExpected)
 	{
-		UCBrow r;
-		r.pos = randMoves[i];
-		r.expected = 0;
-		r.timesPlayed = 0;
-		ucbTable.push_back(r);
+
+	  bestExpected = ucbTable[i].expected;
+	  bestPos = ucbTable[i].pos;
 	}
 
-	//Replay current state to all slaves
-	writeAll(GTP_CMD_CLEAR_BOARD);
-	for(int j = 0; j<gtp.game->Board->movePointer; ++j)
-	{
-		char col = gtp.game->Board->moves[j]->Color == S_BLACK ? 'b' : 'w';
-		std::stringstream ss;
-		ss<<"play "<<col<<" "<<gtp.game->Board->ReadablePosition(gtp.game->Board->moves[j]->Point)<<"\n";
-
-		writeAll(ss.str());
-	}
-
-	std::string wbuf = GTPEngine::generateGTPString(gtp.game->Board);
-	writeAll(wbuf);
-	std::stringstream ss;
-	ss<<"e_randmoves";
-	for(int j = 0;j<randMoves.size();++j)
-		ss<<" "<<randMoves[j];
-	ss<<"\n";
-	writeAll(ss.str());
-	wbuf = "e_useai ucb 0 s 200\ngenmove b";
-	writeAll(wbuf);
-
-	int moveNumber = gtp.game->Board->movePointer; //Implement to prevent race conditions
-
-	for(SocketVector::iterator it = sockets.begin(); it!= sockets.end(); ++it)
-	{
-		//Build errors
-		boost::array<char, MASTER_BUFFER_MAX_SIZE>* buf = new boost::array<char, MASTER_BUFFER_MAX_SIZE>();
-		buf->fill('\0');
-		//		socketreads.push_back(buf);
-		(*it)->socket().async_read_some(boost::asio::buffer(*buf),boost::bind(&MasterServer::genmoveReadCallback,this,(*it),buf));
-	}
-
-	sleep(10);
-
-	int bestPos = -1;
-	float bestExpected = 0;
-	int totalSims = 0;
-	for(int i = 0; i<ucbTable.size(); ++i)
-	{
-		if(ucbTable[i].expected > bestExpected)
-		{
-
-			bestExpected = ucbTable[i].expected;
-			bestPos = ucbTable[i].pos;
-		}
-
-		totalSims+=ucbTable[i].timesPlayed;
-	}
-	LOG_VERBOSE<<"Best move: "<<bestPos<<" based on "<<totalSims<<" simulations"<<std::endl;
-	return gtp.game->Board->ReversePos(bestPos,color);
+      totalSims+=ucbTable[i].timesPlayed;
+    }
+  LOG_VERBOSE<<"Best move: "<<bestPos<<" based on "<<totalSims<<" simulations"<<std::endl;
+  return gtp.game->Board->ReversePos(bestPos,color);
 
 
-	GoPoint pos;
-	gtp.game->Play(pos.color, pos.x,pos.y);
-	LOG_OUT <<"= " << gtp.game->Board->ReadablePosition(pos);
+  GoPoint pos;
+  gtp.game->Play(pos.color, pos.x,pos.y);
+  LOG_OUT <<"= " << gtp.game->Board->ReadablePosition(pos);
 
-	return pos;
+  return pos;
 }
 
 void MasterServer::genmoveReadCallback(boost::shared_ptr<TCPConnection> conn, boost::array<char, 1024>* buf)
 {
-	std::string ucbstring;
-	if(buf->size() != 0 && buf->elems[0] != '\0')
+  std::string input;
+  if(buf->size() != 0 && buf->elems[0] != '\0')
+    {
+      input = std::string(buf->c_array(), strlen(buf->c_array()));
+    }
+  if(!boost::starts_with(input, "= 1"))
+    {
+      //Parse input and confirm UCB table
+      if(input.find("= ucbtable:") != std::string::npos)
 	{
-		ucbstring = std::string(buf->c_array(), strlen(buf->c_array()));
+	  while(writingToUcbTable)
+	    {
+	      LOG_VERBOSE<<"No UCB table consultants are available right now. Please hold while we dig one up"<<std::endl;
+	    }
+	  writingToUcbTable = true;
+	  std::vector<UCBrow> incomingUCBTable = UpperConfidence::parseUCBTableString(input);
+	  UpperConfidence::combineUCBTables(ucbTable, incomingUCBTable);
+	  writingToUcbTable = false;
 	}
-	if(!boost::starts_with(ucbstring, "= 1"))
-	{
-		while(writingToUcbTable)
-		{
-			LOG_VERBOSE<<"No UCB table consultants are available right now. Please hold while we dig one up"<<std::endl;
-		}
-		writingToUcbTable = true;
-		std::vector<UCBrow> incomingUCBTable = UpperConfidence::parseUCBTableString(ucbstring);
-		UpperConfidence::combineUCBTables(ucbTable, incomingUCBTable);
-		writingToUcbTable = false;
-	}
-	buf->fill('\0');
-	conn->socket().async_read_some(boost::asio::buffer(*buf),boost::bind(&MasterServer::genmoveReadCallback,this,conn,buf));
+    }
+  buf->fill('\0');
+  conn->socket().async_read_some(boost::asio::buffer(*buf),boost::bind(&MasterServer::genmoveReadCallback,this,conn,buf));
 }
 
 void MasterServer::writeHandler()
 {
-	//	std::cout<<"Written async"<<std::endl;
+  //	std::cout<<"Written async"<<std::endl;
 }
 
 
--- src/SlaveClient.cpp
+++ src/SlaveClient.cpp
@@ -13,83 +13,88 @@
 #include "Log.h"
 
 SlaveClient::SlaveClient(std::string ip, int port):socket(io_service) {
-	LOG_VERBOSE << "Attempting to connect to "<<ip<<":"<<port;
+  LOG_VERBOSE << "Attempting to connect to "<<ip<<":"<<port;
 
-	try
-	{;
+  try
+    {
 
-	boost::asio::ip::tcp::resolver resolver(io_service);
-	boost::asio::ip::tcp::resolver::query query(ip, "1919");
-	boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-	boost::asio::ip::tcp::resolver::iterator end;
+      boost::asio::ip::tcp::resolver resolver(io_service);
+      boost::asio::ip::tcp::resolver::query query(ip, "1919");
+      boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+      boost::asio::ip::tcp::resolver::iterator end;
 
-
-	boost::system::error_code error = boost::asio::error::host_not_found;
-	while (error && endpoint_iterator != end)
+      boost::system::error_code error = boost::asio::error::host_not_found;
+      while (error && endpoint_iterator != end)
 	{
-		socket.close();
-		socket.connect(*endpoint_iterator++, error);
+	  socket.close();
+	  socket.connect(*endpoint_iterator++, error);
 	}
-	if (error)
-		throw boost::system::system_error(error);
-
-
-	}
-	catch (std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-
+      if (error)
+	throw boost::system::system_error(error);
+    }
+  catch (std::exception& e)
+    {
+      std::cerr << e.what() << std::endl;
+    }
 }
 
-SlaveClient::~SlaveClient() {
-	socket.close();
+SlaveClient::~SlaveClient() 
+{
+  socket.close();
 }
 
 void SlaveClient::run()
 {
 
-	//	    socket.write_
+  //	    socket.write_
 
-	EGTPEngine egtp;
-	LOG_VERBOSE<<"Starting slave"<<std::endl;
-	while(true)
+  EGTPEngine egtp;
+  LOG_VERBOSE<<"Starting slave"<<std::endl;
+  while(true)
+    {
+      try
 	{
-		boost::array<char, SLAVE_BUFFER_MAX_SIZE> buf;
-		buf.fill('\0');
-		boost::system::error_code error;
-		size_t len = socket.read_some(boost::asio::buffer(buf), error);
-		if (error && error != boost::asio::error::eof)
+	  boost::array<char, SLAVE_BUFFER_MAX_SIZE> buf;
+	  buf.fill('\0');
+	  boost::system::error_code error;
+	  size_t len = socket.read_some(boost::asio::buffer(buf), error);
+	  if (error && error != boost::asio::error::eof)
+	    {
+	      LOG_ERROR<<"Socket error: "<<error.message()<<std::endl;
+	      socket.close();
+	      return;
+	    }
+	  size_t bufsize = strlen(buf.data());
+	  if(bufsize>0)
+	    {
+	      if(std::string(buf.data(),bufsize) == EGTP_HANDSHAKE)
 		{
-			LOG_ERROR<<"Socket error: "<<error.message()<<std::endl;
-			socket.close();
-			return;
+		  LOG_DEBUG<<"Got handshake, approved. Sending response"<<std::endl;
+		  socket.write_some(boost::asio::buffer(GTP_ACK_RESPONSE));
 		}
-		size_t bufsize = strlen(buf.data());
-		if(bufsize>0)
+	      else
 		{
-			if(std::string(buf.data(),bufsize) == EGTP_HANDSHAKE)
-			{
-				LOG_DEBUG<<"Got handshake, approved. Sending response"<<std::endl;
-				socket.write_some(boost::asio::buffer(GTP_ACK_RESPONSE));
-			}
-			else
-			{
-				std::string input = std::string(buf.data(),bufsize);
-				std::cout<<"Got input: "<<input<<std::endl;
-				std::vector<std::string> cmds;
-				boost::split(cmds,input, boost::is_any_of( "\n" ));
-				for(std::vector<std::string>::iterator it = cmds.begin(); it != cmds.end(); ++it)
-				{
-					std::string output = egtp.parse(*it);
-					std::cout<<"Sending back: "<<output<<std::endl;
-					socket.write_some(boost::asio::buffer(output));
-				}
+		  std::string input = std::string(buf.data(),bufsize);
+		  std::cout<<"Got input: "<<input<<std::endl;
+		  std::vector<std::string> cmds;
+		  boost::split(cmds,input, boost::is_any_of( "\n" ));
+		  for(std::vector<std::string>::iterator it = cmds.begin(); it != cmds.end(); ++it)
+		    {
+		      std::string output = egtp.parse(*it);
+		      std::cout<<"Sending back: "<<output<<std::endl;
+		      socket.write_some(boost::asio::buffer(output));
+		    }
 
-			}
 		}
-		usleep(100);
+	    }
 	}
-	std::cout<<"No more work. Exiting"<<std::endl;
+      catch (std::exception& e)
+	{
+	  std::cerr << e.what() << std::endl;
+	}
+
+      usleep(100);
+    }
+  std::cout<<"No more work. Exiting"<<std::endl;
 }
 

c49f8f0d258c6039e102663ecd9e3cee929b4ed1
Greblak <greblak@gmail.com>
2013-05-12 22:38:42 +0200

    Added tests according to testplan document CurrentPlayer tests are currently failing
    refs #58 @2h00

--- src/Tests.h
+++ src/Tests.h
@@ -1,4 +1,4 @@
- /*
+/*
  * Tests.h
  *
  *  Created on: Mar 16, 2013
@@ -33,61 +33,234 @@ void TEST_DOTESTS()
   TEST_SimpleRandomAI();  
   std::cout<<"All tests completed successfully"<<std::endl;
 }
-  void TEST_GoBoard_Methods()
-  {
+void TEST_GoBoard_Methods()
+{
 
-    //Testing simple startup and some commands
-    GoBoard* b = new GoBoard(19);
-    assert(b->Size() == 19);
-    assert(!b->Occupied(0)); //Not played at
-    b->Play(0,0);
-    assert(b->Occupied(0)); //Now played at
-    assert(b->IsCorner(0)); // Bottom left corner
-    assert(b->Liberties(0) == 2); // Corner has 2 libs
-    b->Play(1,0);
-    assert(!b->IsCorner(1)); // Right of bottom left corner
-    assert(b->IsBorder(1)); // Bottom border
-    assert(b->Liberties(0) == 3); // Group should have 3 libs
-    assert(b->CurrentPlayer() == S_BLACK); 
-    assert(b->CurrentPlayer() != b->NextPlayer());
-    assert(b->South(0) == -1); //Point does not exist
-    assert(b->West(0) == -1); //Point does not exist
-    assert(b->East(0) == 1); 
-    assert(b->East(18) == -1); //Point does not exist.
-    assert(b->North(0) == 19); 
-    assert(b->IsRealPoint(0));
-    assert(b->IsRealPoint(360)); // Last real point
-    assert(!b->IsRealPoint(361));
-    assert(b->IsRealPoint(19)); 
-    assert(b->IsLegal(2,S_BLACK)); // Empty point
-    assert(!b->IsLegal(0,S_BLACK)); // Occupied point. 
+  //Testing simple startup and some commands
+  //Size()
+GoBoard* b = new GoBoard(9);
+  assert(b->Size() == 9);
+  delete b;
 
-    //Blocktesting
-    assert(b->IsInSameBlock(0,1));
-    assert(!b->IsInSameBlock(0,2));
-    assert(!b->IsInSameBlock(2,1));
-    assert(!b->IsInSameBlock(2,3));
-  }
+  b = new GoBoard(13);
+  assert(b->Size() == 13);
+  delete b;
+
+  b = new GoBoard(19);
+  assert(b->Size() == 19);
+
+b = new GoBoard(9);
+  assert(b->Size() == 9);
+
+  //Occupied
+  assert(!b->Occupied(0)); //Not played at
+  b->Play(0,0);
+  assert(b->Occupied(0)); //Now played at
+  //Check stone removed
+  b->Play(9,1);
+  b->Play(1,1);
+  assert(!b->Occupied(0)); //Now captured
+
+  assert(b->IsCorner(0)); // Bottom left corner
+  assert(!b->IsCorner(1)); // Right of bottom left corner
+  assert(!b->IsCorner(-1)); // invalid
+  assert(!b->IsCorner(22)); // Valid, center
+
+  assert(b->IsBorder(0)); // bottom left corner
+  assert(b->IsBorder(1)); // Bottom border
+  assert(!b->IsBorder(-1)); // Invalid
+  assert(!b->IsBorder(22)); // Not border
+  
+  b->Play(5,1);
+  assert(b->CurrentPlayer() == S_BLACK); //Black after white
+  assert(b->CurrentPlayer() != b->NextPlayer());
+  b->Play(6,0);
+  assert(b->CurrentPlayer() == S_WHITE); 
+
+  //North tests
+  assert(b->North(-1) == -1); 
+  assert(b->North(0) == 9); 
+  assert(b->North(8) == 17);
+  assert(b->North(72) == -1);
+  assert(b->North(80) == -1);
+  assert(b->North(81) == -1);
+  
+//South tests
+  assert(b->South(-1) == -1);
+  assert(b->South(0) == -1);
+  assert(b->South(8) == -1);
+  assert(b->South(72) == 63); 
+  assert(b->South(80) == 71); 
+  assert(b->South(81) == -1); 
+//North tests
+  assert(b->West(-1) == -1);
+  assert(b->West(0) == -1); 
+  assert(b->West(8) == 7); 
+  assert(b->West(72) == -1);
+  assert(b->West(80) == 79);
+  assert(b->West(81) == -1);
+//North tests
+  assert(b->East(-1) == -1);
+  assert(b->East(0) == 1); 
+  assert(b->East(8) == -1);
+  assert(b->East(72) == 73);
+  assert(b->East(80) == -1);
+  assert(b->East(81) == -1);
+ 
+  assert(b->IsRealPoint(0));
+  assert(b->IsRealPoint(360)); // Last real point
+  assert(!b->IsRealPoint(361));
+  assert(b->IsRealPoint(19)); 
+  assert(b->IsLegal(2,S_BLACK)); // Empty point
+  assert(!b->IsLegal(0,S_BLACK)); // Occupied point. 
+
+  //Blocktesting
+  assert(b->IsInSameBlock(0,1));
+  assert(!b->IsInSameBlock(0,2));
+  assert(!b->IsInSameBlock(2,1));
+  assert(!b->IsInSameBlock(2,3));
+}
 void TEST_BoardRepresentation_LibertyCounting()
 {
-  //Testing semi-complicated counting position
-  GoGame g(9);
-  g.Play(S_BLACK,1,1);
-  g.Play(S_BLACK,3,1);
-  g.Play(S_BLACK,2,1);
-  g.Play(S_BLACK,1,2);
-  g.Play(S_WHITE,2,2);
-  g.Play(S_BLACK,3,2);
-  g.Play(S_BLACK,2,3);
-  GoBlock* p_block = g.Board->State.blockPointers[g.Board->Pos(GoPoint(1,1,S_BLACK))];
-  assert(p_block->Liberties() == 10);
+  std::cout<<"***Position A"<<std::endl;
+  {
+    GoGame g(9);
+    g.Play(S_BLACK,0,0);
+    g.Play(S_BLACK,1,0);
+    g.Play(S_BLACK,2,0);
+    g.Play(S_BLACK,3,0);
+    GoBlock* p_block = g.Board->State.blockPointers[g.Board->Pos(GoPoint(0,0,S_BLACK))];
+    assert(p_block->Liberties() == 5);
+  }
 
-  //Testing replay functionality
-  GoGame gg(9);
-  gg.Board->resetAndReplayMoves(g.Board);
-  GoBlock* p_block1 = gg.Board->State.blockPointers[gg.Board->Pos(GoPoint(1,1,S_BLACK))];
-  assert(p_block1->Liberties() == p_block->Liberties());
-  assert(gg.Board->State.stones[gg.Board->Pos(GoPoint(2,2,NONE))] == NONE);
+  std::cout<<"***Position B"<<std::endl;
+  {
+    GoGame g(9);
+    g.Play(S_BLACK,0,1);
+    g.Play(S_BLACK,1,1);
+    g.Play(S_BLACK,2,1);
+    g.Play(S_BLACK,3,1);
+    GoBlock* p_block = g.Board->State.blockPointers[g.Board->Pos(GoPoint(0,1,S_BLACK))];
+    assert(p_block->Liberties() == 9);
+  }
+
+  std::cout<<"***Position C"<<std::endl;
+  {
+    GoGame g = GoGame(9);
+    g.Play(S_BLACK,1,1);
+    g.Play(S_BLACK,2,1);
+    g.Play(S_BLACK,3,1);
+    g.Play(S_BLACK,4,1);
+    GoBlock* p_block = g.Board->State.blockPointers[g.Board->Pos(GoPoint(1,1,S_BLACK))];
+    assert(p_block->Liberties() == 10);
+  }
+
+  std::cout<<"***Position D"<<std::endl;
+  {
+    GoGame g = GoGame(9);
+    g.Play(S_BLACK,1,1);
+    g.Play(S_BLACK,2,1);
+    g.Play(S_BLACK,2,2);
+    g.Play(S_BLACK,2,3);
+    GoBlock* p_block = g.Board->State.blockPointers[g.Board->Pos(GoPoint(1,1,S_BLACK))];
+    assert(p_block->Liberties() == 9);
+  }
+  std::cout<<"***Position E"<<std::endl;
+  {
+    GoGame g = GoGame(9);
+    g.Play(S_BLACK,1,1);
+    g.Play(S_BLACK,2,1);
+    g.Play(S_BLACK,2,2);
+    g.Play(S_BLACK,3,2);
+    g.Play(S_BLACK,2,3);
+    GoBlock* p_block = g.Board->State.blockPointers[g.Board->Pos(GoPoint(1,1,S_BLACK))];
+    assert(p_block->Liberties() == 9);
+  }
+  std::cout<<"***Position F"<<std::endl;
+  {
+    GoGame g = GoGame(9);
+    g.Play(S_BLACK,1,1);
+    g.Play(S_BLACK,2,1);
+    g.Play(S_BLACK,2,2);
+    g.Play(S_BLACK,3,2);
+    g.Play(S_BLACK,1,2);
+    g.Play(S_BLACK,2,3);
+    GoBlock* p_block = g.Board->State.blockPointers[g.Board->Pos(GoPoint(1,1,S_BLACK))];
+    assert(p_block->Liberties() == 9);
+  }
+
+  std::cout<<"***Position G"<<std::endl;
+  {
+    GoGame g = GoGame(9);
+    g.Play(S_BLACK,3,1);
+    g.Play(S_BLACK,2,1);
+    g.Play(S_BLACK,2,3);
+    g.Play(S_BLACK,2,2);
+    g.Play(S_BLACK,4,2);
+    g.Play(S_BLACK,4,3);
+    g.Play(S_BLACK,4,1);
+    GoBlock* p_block = g.Board->State.blockPointers[g.Board->Pos(GoPoint(3,1,S_BLACK))];
+    assert(p_block->Liberties() == 13);
+  }
+ std::cout<<"***Position H"<<std::endl;
+  {
+    GoGame g = GoGame(9);
+    g.Play(S_BLACK,3,1);
+    g.Play(S_WHITE,3,2);
+    g.Play(S_BLACK,3,3);
+    g.Play(S_WHITE,4,2);
+    g.Play(S_BLACK,2,1);
+    g.Play(S_WHITE,3,4);
+    g.Play(S_BLACK,2,3);
+    g.Play(S_WHITE,6,4);
+    g.Play(S_BLACK,2,2);
+    g.Play(S_WHITE,4,1);
+    g.Play(S_BLACK,4,3);
+    g.Play(S_BLACK,5,2);
+    g.Play(S_WHITE,5,4);
+    g.Play(S_BLACK,5,1);
+    g.Play(S_WHITE,2,4);
+    g.Play(S_BLACK,2,5);
+    g.Play(S_BLACK,5,3);
+    g.Play(S_WHITE,7,0);
+    g.Play(S_BLACK,5,0);
+    g.Play(S_WHITE,6,0);
+    g.Play(S_BLACK,4,0);
+    GoBlock* p_block = g.Board->State.blockPointers[g.Board->Pos(GoPoint(3,1,S_BLACK))];
+    assert(p_block->Liberties() == 12);
+  }
+  std::cout<<"***Position I"<<std::endl;
+  {
+    GoGame g = GoGame(9);
+    g.Play(S_BLACK,3,1);
+    g.Play(S_WHITE,3,2);
+    g.Play(S_BLACK,3,3);
+    g.Play(S_WHITE,0,0);
+    g.Play(S_BLACK,2,1);
+    g.Play(S_WHITE,3,4);
+    g.Play(S_BLACK,2,3);
+    g.Play(S_WHITE,6,4);
+    g.Play(S_BLACK,2,2);
+    g.Play(S_WHITE,4,1);
+    g.Play(S_BLACK,4,3);
+    g.Play(S_BLACK,5,2);
+    g.Play(S_WHITE,5,4);
+    g.Play(S_BLACK,5,1);
+    g.Play(S_WHITE,2,4);
+    g.Play(S_BLACK,2,5);
+    g.Play(S_BLACK,5,3);
+    g.Play(S_WHITE,7,0);
+    g.Play(S_BLACK,5,0);
+    g.Play(S_WHITE,6,0);
+    g.Play(S_BLACK,4,0);
+    g.Play(S_WHITE,0,1);
+    g.Play(S_BLACK,4,2);
+    GoBlock* p_block = g.Board->State.blockPointers[g.Board->Pos(GoPoint(3,1,S_BLACK))];
+    assert(p_block->Liberties() == 11);
+  }
+ 
+
+
 }
 void TEST_SimpleRandomAI()
 {
